108dccf34bdfb79d25a89c9803c79206e627475d
Julianafatsil <julianafatsil@gmail.com>
2020-08-04 15:36:06 -0300

    RevisionIssues

--- src/Helper/renderWithRedux.js
+++ src/Helper/renderWithRedux.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
+
 import rootReducers from '../redux/reducers';
+
 const renderWithRedux = (
   component,
-  { initialState, store = createStore(rootReducers, applyMiddleware(thunk)) } = {},
-) => {
-  return {
-    ...render(<Provider store={store}>{component}</Provider>),
-    store,
-  };
-};
+  { initialState, store = createStore(rootReducers, initialState) } = {},
+) => ({
+  ...render(<Provider store={store}>{component}</Provider>),
+  store,
+});
+
 export default renderWithRedux;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
 import store from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
