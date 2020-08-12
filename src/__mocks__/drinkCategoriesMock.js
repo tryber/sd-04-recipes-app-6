d3ddf9ac0d92ed881fad495fa234a4a853e1c47c
Victor L <victor.lustosacp@gmail.com>
2020-08-12 13:18:34 -0300

    Aplicaçao do Mock

---
+++ src/__mocks__/drinkCategoriesMock.js
@@ -0,0 +1,18 @@
+module.exports = {
+  meals: [
+    { strCategory: 'Beef' },
+    { strCategory: 'Breakfast' },
+    { strCategory: 'Chicken' },
+    { strCategory: 'Dessert' },
+    { strCategory: 'Goat' },
+    { strCategory: 'Lamb' },
+    { strCategory: 'Miscellaneous' },
+    { strCategory: 'Pasta' },
+    { strCategory: 'Pork' },
+    { strCategory: 'Seafood' },
+    { strCategory: 'Side' },
+    { strCategory: 'Starter' },
+    { strCategory: 'Vegan' },
+    { strCategory: 'Vegetarian' },
+  ],
+};
--- src/__tests__/Footer.test.js
+++ src/__tests__/Footer.test.js
@@ -4,10 +4,24 @@ import renderWithRouter from '../Helper/renderWithRouter';
 import renderWithRedux from '../Helper/renderWithRedux';
 import Footer from '../components/Footer';
 import App from '../App';
+import getRecipesApi from '../services/getRecipesApi';
+import foodRecipesMock from '../__mocks__/foodRecipesMock';
+import foodCategoriesMock from '../__mocks__/foodCategories.Mock';
+import drinkRecipesMock from '../__mocks__/drinkRecipesMock';
+import drinkCategoriesMock from '../__mocks__/drinkCategoriesMock';
 
 afterEach(() => {
   cleanup;
 });
+jest.mock('../services/getRecipesApi');
+
+getRecipesApi.mockImplementationOnce(() => Promise.resolve(foodRecipesMock));
+getRecipesApi.mockImplementationOnce(() => Promise.resolve(foodCategoriesMock));
+getRecipesApi.mockImplementationOnce(() => Promise.resolve(drinkRecipesMock));
+getRecipesApi.mockImplementationOnce(() => Promise.resolve(drinkCategoriesMock));
+getRecipesApi.mockImplementationOnce(() => Promise.resolve(foodRecipesMock));
+getRecipesApi.mockImplementationOnce(() => Promise.resolve(foodCategoriesMock));
+
 
 describe('Testes component Footer', () => {
   test('testando Botoes', () => {
@@ -21,13 +35,14 @@ describe('Testes component Footer', () => {
   })
   test('Click dos botoes', async () => {
     const { getByText, getByTestId } = renderWithRedux(renderWithRouter(<App />, ['/comidas']));
+    await waitForDomChange();
     expect(getByText('Comidas')).toBeInTheDocument();
     const bebidas = getByTestId('drinks-bottom-btn');
     fireEvent.click(bebidas);
     await waitForDomChange(() => expect(getByText('Bebidas')).toBeInTheDocument());
-    expect(getByText('Bebidas')).toBeInTheDocument();
+    expect(getByTestId('0-recipe-card')).toBeInTheDocument();
     const comidas = getByTestId('food-bottom-btn');
     fireEvent.click(comidas);
     await waitForDomChange(() => expect(getByText('Comidas')).toBeInTheDocument());
-  })
+   })
 })
