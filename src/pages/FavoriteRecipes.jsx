2b969e8190d8ae1726890759824c3b31b4aa0522
julianafatsil <julianafatsil@gmail.com>
2020-08-18 02:57:18 -0300

    Revision Issues

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import Image from '../components/Image';
@@ -11,49 +12,70 @@ const getDescriptionFavorite = (favorite) =>
     ? `${favorite.category} - ${favorite.area}`
     : favorite.alcoholicOrNot;
 
-const filterFavorites = (type, dataFavorites) => (
-  type === 'All' ? dataFavorites : dataFavorites.filter(favorite => (
-    favorite.type === type
-    )))
+const filterFavorites = (type, dataFavorites) =>
+  type === 'All'
+    ? dataFavorites
+    : dataFavorites.filter((favorite) => favorite.type === type);
 
-function FavoriteRecipes () { 
-  const dataFavorites  = getLocalStorage('favoriteRecipes');
-// 
+function FavoriteRecipes() {
+  const dataFavorites = getLocalStorage('favoriteRecipes');
+
   const [typeFilter, setTypeFilter] = useState('All');
   const favoritesAll = filterFavorites(typeFilter, dataFavorites);
 
-    return (
-      <div>
-        <Header title={"Receitas Favoritas"}/>
-        <div className="favorite">
-          <div className="favorite-category">
-            <Button onClick={()=>{
-              setTypeFilter('All')
-            }}>All</Button>
-            <Button onClick={()=>{
-              setTypeFilter('comida')
-            }}>Food</Button>
-            <Button onClick={()=>{
-              setTypeFilter('bebida')
-            }}>Drinks</Button>
-          </div>
-          {favoritesAll.map((favorite) =>(
-            <div className="favorite-card" key={favorite.name}>
-              <div className="favorite-card-details">
-                <Image to={''} src={favorite.image} alt={favorite.name} />
-              </div>
-              <div className="favorite-card-details">
-                <span>{getDescriptionFavorite(favorite)}</span>
-                <h1>{favorite.name}</h1>
-                <TitleAndButtons alcoholicOrNot={favorite.alcoholicOrNot}title={favorite.name}
-                area={favorite.area}image={favorite.image}type={favorite.type}
-                id={favorite.id} category={favorite.category} />
-              </div>
-            </div>
-          ))}
+  return (
+    <div>
+      <Header title={'Receitas Favoritas'} />
+      <div className="favorite">
+        <div className="favorite-category">
+          <Button
+            onClick={() => {
+              setTypeFilter('All');
+            }}
+          >
+            All
+          </Button>
+          <Button
+            onClick={() => {
+              setTypeFilter('comida');
+            }}
+          >
+            Food
+          </Button>
+          <Button
+            onClick={() => {
+              setTypeFilter('bebida');
+            }}
+          >
+            Drinks
+          </Button>
         </div>
+        {favoritesAll.map((favorite, index) => (
+            <Link to={`/comidas/${favorite.id}`}>
+          <div className="favorite-card" key={favorite.name}>
+            <div className="favorite-card-details">
+              <Image to={`/comidas/${favorite.id}`} src={favorite.image} alt={favorite.name} />
+            </div>
+            <div className="favorite-card-details">
+              <span>{getDescriptionFavorite(favorite)}</span>
+              <h1>{favorite.name}</h1>
+              <TitleAndButtons
+                alcoholicOrNot={favorite.alcoholicOrNot}
+                title={favorite.name}
+                area={favorite.area}
+                image={favorite.image}
+                type={favorite.type}
+                id={favorite.id}
+                category={favorite.category}
+                data-testid={`${index}-horizontal-favorite-btn`}
+              />
+            </div>
+          </div>
+          </Link>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default FavoriteRecipes;
