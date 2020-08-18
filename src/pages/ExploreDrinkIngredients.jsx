e740d3dc808ee27646c923a06cb0ba4d1b511f82
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-08-18 13:53:01 -0300

    ok

--- src/pages/ExploreDrinkIngredients.jsx
+++ src/pages/ExploreDrinkIngredients.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import getRecipesAPI from '../services/getRecipesApi';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function ExploreDrinkIngredients() {
   const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
@@ -11,15 +13,18 @@ function ExploreDrinkIngredients() {
 
   return (
     <div>
-      {ingredients.map((el) => (
+      <Header title="Explorar Ingredientes" searchEnabled={false}/>
+      {ingredients.map((el, i) => (
         <div>
-          <p>{el.strIngredient1}</p>
+          <p data-testid={`${i}-card-name`}>{el.strIngredient1}</p>
           <img
             src={`https://www.thecocktaildb.com/images/ingredients/${el.strIngredient1}-Small.png`}
             alt="thumbnail"
+            data-testid={`${i}-card-img`}
           />
         </div>
       ))}
+      <Footer />
     </div>
   );
 }
--- src/pages/ExploreFoodIngredients.jsx
+++ src/pages/ExploreFoodIngredients.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import getRecipesAPI from '../services/getRecipesApi';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function ExploreFoodIngredients() {
   const url = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
@@ -7,19 +9,21 @@ function ExploreFoodIngredients() {
   useEffect(() => {
     getRecipesAPI(url).then((data) => setIngredients(data.meals));
   }, []);
-  console.log(ingredients);
 
   return (
-    <div>
-      {ingredients.map((el) => (
+    <div className="food-ingredients" >
+      <Header />
+      {ingredients.map((el, i) => (
         <div>
-          <p>{el.strIngredient}</p>
+          <p data-testid={`${i}-card-name`}>{el.strIngredient}</p>
           <img
             src={`https://www.themealdb.com/images/ingredients/${el.strIngredient}.png`}
             alt="thumbnail"
+            data-testid={`${i}-card-img`}
           />
         </div>
       ))}
+      <Footer />
     </div>
   );
 }
